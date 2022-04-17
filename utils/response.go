951eddfbad2ec7fe7ba168fe3dfa8c800208b60f
thanhtuan472k <tuango.472000@gmail.com>
2022-04-17 22:55:18 +0700

    add func 401 response

--- utils/response.go
+++ utils/response.go
@@ -1,34 +1,42 @@
-package responses
+package utils
 
 import (
-	"net/http"
-
 	"github.com/labstack/echo/v4"
+	"net/http"
 )
 
-type Response map[string] interface{}
+type Response map[string]interface{}
 
 func generateResponse(data interface{}, message string) Response {
-    return Response{
-        "data": data,
-        "message": message,
-    }
+	return Response{
+		"data":    data,
+		"message": message,
+	}
 }
 
 // Response200 success
 func Response200(c echo.Context, data interface{}, message string) error {
-    if message == "" {
-        message = "Thanh Cong"
-    }
-    return c.JSON(http.StatusBadRequest, generateResponse(data, message))
+	if message == "" {
+		message = "Thanh Cong"
+	}
+	return c.JSON(http.StatusBadRequest, generateResponse(data, message))
 }
 
-// Response400 b9ad request
- func Response400(c echo.Context, data interface{}, message string) error {
+// Response400 bad request
+func Response400(c echo.Context, data interface{}, message string) error {
 	if message == "" {
 		message = "Dữ liệu không hợp lệ"
 	}
 	return c.JSON(http.StatusBadRequest, generateResponse(data, message))
+}
+
+// Response401 unauthorized
+func Response401(c echo.Context, data interface{}, message string) error {
+
+	if message == "" {
+		message = "unauthorized"
+	}
+	return c.JSON(http.StatusUnauthorized, generateResponse(data, message))
 }
 
 // Response404 not found
